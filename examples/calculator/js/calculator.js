e3492d6aeca9cee69520b3c31fba2b0dbe6e730e
natasha <znatashka@ya.ru>
2019-01-23 21:09:14 +0300

    +
    (cherry picked from commit e76923d629df92960280c8651c6d18bee4fdb2f2)

--- examples/calculator/js/calculator.js
+++ examples/calculator/js/calculator.js
@@ -1,131 +1,83 @@
-class Base {
+class Calculator {
     constructor(element) {
         this._element = element;
-    }
+        this._element.innerHTML = this._html();
 
-    render() {
-        throw new Error('You have to implement the method `render`!');
-    }
-}
-
-class Calculator extends Base {
-    constructor(element) {
-        super(element);
-        this._inputField = new InputField(element);
-        this._equalsButton = new EqualsButton(element);
-        this._numericAndMemoryButtons = new NumericAndMemoryButtons(element, this._inputField);
-        this._functionButtons = new FunctionButtons(element);
-        this._element.innerHTML = this.render();
-    }
-
-    render() {
-        return `
-            <div class="calc">
-                ${this._inputField.render()}
-                ${this._equalsButton.render()}
-                
-                <div class="buttons">
-                    ${this._numericAndMemoryButtons.render()}                    
-                    ${this._functionButtons.render()}
-                </div>
-            </div>
-        `;
-    }
-}
-
-class InputField extends Base {
-    constructor(element) {
-        super(element);
-    }
-
-    render() {
-        return `<input class="calc__input-field" type="text" placeholder="0"><span></span></input>`;
-    }
-
-    element() {
-        return this._element.querySelector('.calc__input-field');
-    }
-}
-
-class EqualsButton extends Base {
-    constructor(element) {
-        super(element);
-    }
-
-    render() {
-        return `<div class="calc__btn btn__equals">=</div>`;
-    }
-}
-
-class FunctionButtons extends Base {
-    constructor(element) {
-        super(element);
-        this._functions = {
-            '+': (a, b) => a + b,
-            '-': (a, b) => a - b,
-            '*': (a, b) => a * b,
-            '/': (a, b) => a / b
-        }
-    }
-
-    render() {
-        return `
-            <div class="calc__function-buttons">
-                ${
-            [...Object.keys(this._functions)]
-                .map(f => `<div class="calc__btn">${f}</div>`).join('')
-            }
-            </div>
-        `;
-    }
-}
-
-class NumericAndMemoryButtons extends Base {
-    constructor(element, inputField) {
-        super(element);
-        this._inputField = inputField;
-        this._labels = {
-            '#1': () => this._inputFieldAppend(1),
-            '#2': () => this._inputFieldAppend(2),
-            '#3': () => this._inputFieldAppend(3),
-            '#4': () => this._inputFieldAppend(4),
-            '#5': () => this._inputFieldAppend(5),
-            '#6': () => this._inputFieldAppend(6),
-            '#7': () => this._inputFieldAppend(7),
-            '#8': () => this._inputFieldAppend(8),
-            '#9': () => this._inputFieldAppend(9),
-            '#M+': () => this._putToMemory(),
-            '#0': () => this._inputFieldAppend(0),
-            '#M-': () => this._getFromMemory()
-        };
-        this._element.addEventListener('click', (event) => {
-            if (event.target.classList.contains('calc__btn')) {
-                this._labels['#'.concat(event.target.innerText)]();
-            }
+        this._element.addEventListener('click', event => {
+            new ClearButtonClick({element: this._element, event: event}).action();
+            new CalcButtonClick({element: this._element, event: event}).action();
+            new EqualsButtonClick({element: this._element, event: event}).action();
         });
     }
 
-    render() {
+    _html() {
         return `
-            <div class="calc__numeric-buttons">
-                ${
-            [...Object.keys(this._labels)]
-                .map(label =>
-                    `<div class="calc__btn">${label.split('#')[1]}</div>`).join('')
-            }
-            </div>
-        `;
+            <div class="calc">
+                <input class="calc__input-field" type="text" placeholder="0">
+                <span class="calc__clear-btn"></span>
+                <div class="btn calc__equals-btn" data-label="="></div>
+                
+                <div class="buttons">
+                    <div class="calc__numeric-buttons">
+                        ${[...Array(9).keys()].map((_, i) => `<div class="btn calc__btn" data-label="${i + 1}"></div>`).join('')}
+                        <div class="btn calc__memory-btn" data-label="M+"></div>
+                        <div class="btn calc__btn" data-label="0"></div>
+                        <div class="btn calc__memory-btn" data-label="M-"></div>
+                    </div>                 
+                    
+                    <div class="calc__function-buttons">
+                        ${[...'+-*/'].map(f => `<div class="btn calc__btn" data-label="${f}"></div>`).join('')}
+                    </div>
+                    </div>
+                </div>`;
+    }
+}
+
+class BaseClick {
+    constructor({element, event}) {
+        this._element = element;
+        this._event = event;
     }
 
-    _inputFieldAppend(part) {
-        this._inputField.element().value = this._inputField.element().value.concat(part)
+    action() {
+        throw new Error('Do nothing!')
+    }
+}
+
+class ClearButtonClick extends BaseClick {
+    constructor(options) {
+        super(options);
     }
 
-    _putToMemory() {
+    action() {
+        if (this._event.target.classList.contains('calc__clear-btn')) {
+            this._element.querySelector('.calc__input-field').value = '';
+        }
+    }
+}
 
+class CalcButtonClick extends BaseClick {
+    constructor(options) {
+        super(options);
     }
 
-    _getFromMemory() {
+    action() {
+        if (this._event.target.classList.contains('calc__btn')) {
+            this._element.querySelector('.calc__input-field').value =
+                this._element.querySelector('.calc__input-field').value.concat(this._event.target.dataset.label);
+        }
+    }
+}
 
+class EqualsButtonClick extends BaseClick {
+    constructor(options) {
+        super(options);
+    }
+
+    action() {
+        if (this._event.target.classList.contains('calc__equals-btn')) {
+            const expression = this._element.querySelector('.calc__input-field').value;
+            console.log(expression.split(''))
+        }
     }
 }
