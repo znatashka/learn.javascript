0c2eb13815c902f6f586d0f4999fe5db7483db59
Natalia Zolotovitskaya <natalia.zolotovitskaya@infotecs.ru>
2019-01-23 17:00:50 +0300

    +
    (cherry picked from commit c43a94a6421d37bbc140cb7dbbb165f8304fd0aa)

--- examples/calculator/js/calculator.js
+++ examples/calculator/js/calculator.js
@@ -1,14 +1,24 @@
-class Calculator {
+class Base {
     constructor(element) {
-        this._inputField = new InputField();
-        this._equalsButton = new EqualsButton();
-        this._numericAndMemoryButtons = new NumericAndMemoryButtons();
-        this._functionButtons = new FunctionButtons();
-
-        element.innerHTML = this._render();
+        this._element = element;
     }
 
-    _render() {
+    render() {
+        throw new Error('You have to implement the method `render`!');
+    }
+}
+
+class Calculator extends Base {
+    constructor(element) {
+        super(element);
+        this._inputField = new InputField(element);
+        this._equalsButton = new EqualsButton(element);
+        this._numericAndMemoryButtons = new NumericAndMemoryButtons(element, this._inputField);
+        this._functionButtons = new FunctionButtons(element);
+        this._element.innerHTML = this.render();
+    }
+
+    render() {
         return `
             <div class="calc">
                 ${this._inputField.render()}
@@ -23,19 +33,23 @@ class Calculator {
     }
 }
 
-class InputField {
-    constructor() {
-
+class InputField extends Base {
+    constructor(element) {
+        super(element);
     }
 
     render() {
-        return `<input class="calc__input-field" type="text" placeholder="0">`;
+        return `<input class="calc__input-field" type="text" placeholder="0"><span></span></input>`;
+    }
+
+    element() {
+        return this._element.querySelector('.calc__input-field');
     }
 }
 
-class EqualsButton {
-    constructor() {
-
+class EqualsButton extends Base {
+    constructor(element) {
+        super(element);
     }
 
     render() {
@@ -43,8 +57,9 @@ class EqualsButton {
     }
 }
 
-class FunctionButtons {
-    constructor() {
+class FunctionButtons extends Base {
+    constructor(element) {
+        super(element);
         this._functions = {
             '+': (a, b) => a + b,
             '-': (a, b) => a - b,
@@ -56,22 +71,61 @@ class FunctionButtons {
     render() {
         return `
             <div class="calc__function-buttons">
-                ${[...Object.keys(this._functions)].map(f => `<div class="calc__btn">${f}</div>`).join('')}
+                ${
+            [...Object.keys(this._functions)]
+                .map(f => `<div class="calc__btn">${f}</div>`).join('')
+            }
             </div>
         `;
     }
 }
 
-class NumericAndMemoryButtons {
-    constructor() {
-        this._labels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'M+', 0, 'M-'];
+class NumericAndMemoryButtons extends Base {
+    constructor(element, inputField) {
+        super(element);
+        this._inputField = inputField;
+        this._labels = {
+            '#1': () => this._inputFieldAppend(1),
+            '#2': () => this._inputFieldAppend(2),
+            '#3': () => this._inputFieldAppend(3),
+            '#4': () => this._inputFieldAppend(4),
+            '#5': () => this._inputFieldAppend(5),
+            '#6': () => this._inputFieldAppend(6),
+            '#7': () => this._inputFieldAppend(7),
+            '#8': () => this._inputFieldAppend(8),
+            '#9': () => this._inputFieldAppend(9),
+            '#M+': () => this._putToMemory(),
+            '#0': () => this._inputFieldAppend(0),
+            '#M-': () => this._getFromMemory()
+        };
+        this._element.addEventListener('click', (event) => {
+            if (event.target.classList.contains('calc__btn')) {
+                this._labels['#'.concat(event.target.innerText)]();
+            }
+        });
     }
 
     render() {
         return `
             <div class="calc__numeric-buttons">
-                ${this._labels.map(label => `<div class="calc__btn">${label}</div>`).join('')}
+                ${
+            [...Object.keys(this._labels)]
+                .map(label =>
+                    `<div class="calc__btn">${label.split('#')[1]}</div>`).join('')
+            }
             </div>
         `;
+    }
+
+    _inputFieldAppend(part) {
+        this._inputField.element().value = this._inputField.element().value.concat(part)
+    }
+
+    _putToMemory() {
+
+    }
+
+    _getFromMemory() {
+
     }
 }
