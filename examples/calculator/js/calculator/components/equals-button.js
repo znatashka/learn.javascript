ef3a91dcd453674bc90bc293a5b551ae00dc5d9c
natasha <znatashka@ya.ru>
2019-01-27 13:32:50 +0300

    +
    (cherry picked from commit a5facd316611b354699d6730ade080fa22d3f71f)

--- examples/calculator/js/calculator/components/equals-button.js
+++ examples/calculator/js/calculator/components/equals-button.js
@@ -13,16 +13,53 @@ export default class EqualsButton extends BaseComponent {
     }
 
     _calculate() {
-        let expression = this._onReadValue();
-        console.log('calculating an expression: ', expression);
-
-        expression = [...expression]
+        const expression = [...this._onReadValue()]
             .map(s => this._operationsKeys.includes(s) ? ` ${s} ` : s)
             .join('')
             .split(' ');
 
-        // todo https://habr.com/ru/post/282379/
-        console.log(expression);
+        this._onWriteValue(
+            this._rpn(expression)
+        );
+    }
+
+    _rpn(expression) {
+        const stack = [];
+        const output = [];
+        const result = [];
+
+        expression.forEach(part => {
+            if (this._operationsKeys.includes(part)) {
+                if (stack.length === 0) {
+                    stack.push(part);
+                } else {
+                    const last = stack[stack.length - 1];
+                    if (this._operations[last].priority < this._operations[part].priority) {
+                        output.push(stack.pop());
+                    }
+                    stack.push(part);
+                }
+            } else {
+                output.push(part);
+            }
+        });
+
+        while (stack.length > 0) {
+            output.push(stack.pop());
+        }
+
+        output.forEach(part => {
+            if (this._operationsKeys.includes(part)) {
+                const b = +result.pop();
+                const a = +result.pop();
+                result.push(
+                    this._operations[part].func(a, b)
+                );
+            } else {
+                result.push(part);
+            }
+        });
+        return result.pop();
     }
 
     _render() {
